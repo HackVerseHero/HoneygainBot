b810c0ee20d6dc1014804ca438d6a86ac74530dc
HackVerseHero <nickolaihula@gmail.com>
2023-07-30 01:05:18 +0300

    The "Storage" folder will be created automatically if it is absent.

--- honeygain.py
+++ honeygain.py
@@ -6,7 +6,10 @@ from Database.sqlite import SQLiteStorage
 USER = os.environ['HONEYGAIN_USER']
 PASS = os.environ['HONEYGAIN_PASS']
 
-database_file = os.path.join("Storage", "data.db")
+dir_name = 'Storage'
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+database_file = os.path.join(dir_name, 'data.db')
 
 storage = SQLiteStorage(database_file)
 hg = HoneypotCatchBot(USER, PASS, storage)
